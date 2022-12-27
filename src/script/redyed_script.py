6f98e6e6deb5c19d4a3af226bb2ceae39aaad9c7
EvanHsieh0415 <evan940415@gmail.com>
2022-12-27 15:29:17 +0800

    feat: redyed script

--- src/script/redyed_script.py
+++ src/script/redyed_script.py
@@ -8,4 +8,38 @@ import json
 with open(path_join('..', 'base', 'minecraft', 'color.json')) as file:
     colors = json.load(file)
 
-base_json = lambda color: 
+base_json = lambda color, tag, modid, item_id: {
+    "forge:conditions": [
+        {
+            "type": "forge:mod_loaded",
+            "mod_id": "create"
+        },
+        {
+            "type": "forge:mod_loaded",
+            "mod_id": modid
+        }
+    ],
+    "fabric:load_conditions": [
+        {
+        "condition": "fabric:all_mods_loaded",
+        "values": [
+            "create",
+            modid
+            ]
+        }
+    ],
+    "type": "create:splashing",
+    "ingredients": [
+        {
+            "tag": tag,
+            "count": 1
+        }
+    ],
+    "results": [
+        {
+            "item": f"{modid}:{color}_{item_id}",
+            "count": 1,
+            "chance": 1
+        }
+    ]
+}
